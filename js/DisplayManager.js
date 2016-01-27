5b302981b1dcb8a07ac5fa2f45f94d406c83cbbe
Vincent <vl.emile@gmail.com>
2016-01-27 15:30:14 +0100

    [Display Manager]display Manager is now jshint valid

--- js/DisplayManager.js
+++ js/DisplayManager.js
@@ -1,7 +1,13 @@
-function DisplayManager(){
-	this.display_messagebox = function(title, text){
-		$("#messagebox_title").text(title);
-		$("#messagebox_text").text(text);
-		$("#messagebox").show();
-	}
+/*global
+    $
+ */
+/*jshint camelcase: false*/
+
+function DisplayManager() {
+    "use strict";
+    this.display_messagebox = function (title, text) {
+        $("#messagebox_title").text(title);
+        $("#messagebox_text").text(text);
+        $("#messagebox").show();
+    };
 }
